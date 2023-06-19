d17e931d97f41e3729956950fe3c5cffa859887b
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-06-19 14:41:51 +0100

    added final grass card and renamed the cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import waterCard from "./assets/water-card.gif";
+import fireCard from "./assets/fire-card.gif";
+import grassCard from "./assets/grass-card.gif";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cardsContainer">
+      <div>
+        {/* prettier-ignore */}
+        <img id="waterCardImg" className="elemCards" src={waterCard} alt="water card" />
+      </div>
+      <div>
+        {/* prettier-ignore */}
+        <img id="fireCardImg"  className="elemCards"src={fireCard} alt="fire card" />
+      </div>
+      <div>
+        {/* prettier-ignore */}
+        <img id="grassCardImg"  className="elemCards"src={grassCard} alt="grass card" />
+      </div>
     </div>
   );
 }
