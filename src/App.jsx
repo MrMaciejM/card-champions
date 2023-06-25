8087df7931f4d69eedbd5411088c52952129903b
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-06-25 08:26:48 +0100

    added sound effects for win/loss round, and card battle area which will be animated

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import waterCard from "./assets/water-card.gif";
 import fireCard from "./assets/fire-card.gif";
 import grassCard from "./assets/grass-card.gif";
 import cardDeck from "./assets/card-deck.gif";
+import testCard from "./assets/placeholder-card.png";
 
 // sound assets
 import waterSound from "./assets/Water-bubbles.mp3";
 import fireSound from "./assets/Fire-intense.mp3";
 import grassSound from "./assets/Leaves-rustle.mp3";
+import scoreSound from "./assets/score-sound.mp3";
+import zapSound from "./assets/zap-effect.mp3";
 
 
 function App() {
@@ -21,6 +24,7 @@ function App() {
   const [fireValue] = useState("fire");
   const [grassValue] = useState("grass");
   
+  // sound effect for cards
   function soundEffect(element) {
     switch(element) {
       case "water":
@@ -43,7 +47,26 @@ function App() {
     }
   }
 
-  // value depends on the card clicked
+  // sounds for won and loss rounds
+  function wonRoundSound() {
+    const wonSound = new Audio(scoreSound);
+    wonSound.volume = 1;
+    setTimeout(() => {
+      wonSound.play();
+    }, 3000);
+    
+  }
+  function lostRoundSound() {
+    const lossRound = new Audio(zapSound);
+    lossRound.volume = 1;
+    setTimeout(() => {
+      lossRound.play();
+    }, 3000);
+    
+  }
+
+
+  // player choice - value depends on the card clicked
   const handlePlayerChoice = (value) => {
     cpuLogic(value);
     soundEffect(value);
@@ -55,9 +78,7 @@ function App() {
     let randomiser = Math.floor(Math.random() * 3);
     let cpuChoice = elements[randomiser]; 
     console.log("player choice: " + playerChoice);
-    //console.log("player 1 choice: " + playerChoice);
     console.log("rnd result: " + elements[randomiser]);
-    //console.log(cpuChoice);
 
     if (playerChoice === cpuChoice) {
       console.log("DRAW!");
@@ -65,27 +86,33 @@ function App() {
     }
     else if (playerChoice === "water" && cpuChoice === "grass") {
       cpuScore++;
+      lostRoundSound()
       gameScore();
     }
     else if (playerChoice === "water" && cpuChoice === "fire") {
       console.log("Player score var = " + playerScore);
       playerScore++;
+      wonRoundSound();
       gameScore(); 
     }
     else if (playerChoice === "fire" && cpuChoice === "grass") {
       playerScore++;
+      wonRoundSound();
       gameScore(); 
     }
     else if (playerChoice === "fire" && cpuChoice === "water") {
       cpuScore++;
+      lostRoundSound();
       gameScore(); 
     }
     else if (playerChoice === "grass" && cpuChoice === "water") {
       playerScore++;
+      wonRoundSound();
       gameScore(); 
     }
     else if (playerChoice === "grass" && cpuChoice === "fire") {
       cpuScore++;
+      lostRoundSound();
       gameScore(); 
     }
   }
@@ -96,30 +123,37 @@ function App() {
     playerScoreElement.textContent = `Player 1: ${playerScore}`; 
     cpuScoreElement.textContent = `Player 2: ${cpuScore}`; 
   }
+
+  // date for copyright 
+  const currentDate = new Date();
+  const dateFormatted = currentDate.getFullYear(); 
   
   return (
     <main>
       <h1>Card Champions</h1>
       <h2>Score</h2>
       <div className="playerScoresDiv">
-      <h3 id="playerScore">Player 1: {playerScore}</h3>
-      <h3 id="cpuScore">Player 2: {cpuScore}</h3>
+      <h3 id="playerScore">Challenger: {playerScore}</h3>
+      <h3 id="cpuScore">Card Champion: {cpuScore}</h3>
       </div>
 
       <div className="cardsContainer">
-        <div>
+        <div id="tripleCardsDiv">
           {/* prettier-ignore */}
           <img id="waterCardImg" className="elemCards" src={waterCard} alt="water card" onClick={() => handlePlayerChoice(waterValue)} />
-
           <img id="fireCardImg" className="elemCards" src={fireCard} alt="fire card" onClick={() => handlePlayerChoice(fireValue)} />
-
           <img id="grassCardImg" className="elemCards" src={grassCard} alt="grass card" onClick={() => handlePlayerChoice(grassValue)} />
         </div>
-        <div>
+          <div id="cardBattleDiv">
+          <img className="testCard" alt="aa" src={testCard}/>
+          <img className="testCard" alt="aa" src={testCard}/>
+          </div>
+        <div className="cardDeckDiv">
           {/* prettier-ignore */}
           <img id="cardDeckImg" className="elemCards" src={cardDeck} alt="water card" />
         </div>
       </div>
+      <p className="copyright">Copyright © {dateFormatted} - Maciej</p>
     </main>
   );
 }
